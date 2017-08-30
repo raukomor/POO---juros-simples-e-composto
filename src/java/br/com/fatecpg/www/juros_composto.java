9d8f31b48ed4969bd231ec40e53cd9b4cf9e51e2
Rodrigo <Rodrigo@DELLXPS>
2017-08-30 10:03:42 -0300

    Atualizaçãojuros compostos

--- src/java/br/com/fatecpg/www/juros_composto.java
+++ src/java/br/com/fatecpg/www/juros_composto.java
@@ -7,6 +7,7 @@ package br.com.fatecpg.www;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,14 +38,58 @@ public class juros_composto extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<style>");
-            out.println("body{background-color:rgb(210,205,230);}");
-            out.println("h1{text-align: center;font-family:arial;}");
-            out.println("</style>");
-            out.println("<title>Servlet juros_composto</title>");            
+            out.println("<title>Servlet juros compostos</title>");            
+            out.println("<link rel='stylesheet' type='text/css' href='" + request.getContextPath() +"/styles/design.css' />");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Cálculo de Juros Composto</h1>");
+            out.println("<div>");
+            out.println("<a href='home'>");
+            out.println("<span class='home' title='Voltar Para o Home' href='/home'>");
+            out.println("<img src='" + request.getContextPath() +"/images/home.png' />");
+            out.println("</span>");
+            out.println("</a>");
+            out.println("<form>");
+            out.println("<h1>Juros Compostos</h1>");
+            out.println("<input type='number' name='capital' placeholder='Capital' step=0.01  required/>");
+            out.println("<input type='number' name='taxa' placeholder='Taxa de Juros' step=0.01  required/>");
+            out.println("<input type='number' name='periodo' placeholder='Período(Meses)' required/>");
+            out.println("<input type='submit' value='Calcular'/>");
+            out.println("</form>");
+            out.println("<br/>");
+            out.println("<br/>");
+            double capital=Double.parseDouble(request.getParameter("capital"));
+            double taxa=Double.parseDouble(request.getParameter("taxa"))/100;
+            double periodo=Double.parseDouble(request.getParameter("periodo"));
+            double prest=0;
+            double juros=0;
+            double amort=0;
+            double saldo_dev=capital;
+            if (capital>0 && taxa>0 && periodo>0){
+                out.println("<h2 class=\"result\">Resultado</h2>");
+                prest=capital/((1-Math.pow((1+taxa),-periodo))/taxa);
+                out.println("<table cellpadding=\"10\">");
+                out.println("<tr class='tabela_titulo'><th>Parcela</th><th>Saldo devedor</th><th>Amortização</th><th>Juros</th><th>Prestação</th></tr>");
+                for (int i = 1; i <= periodo; i++) {
+                    juros=saldo_dev*taxa;
+                    amort=prest-juros;
+                    saldo_dev=saldo_dev-amort;
+                    out.print("<tr class=\"tabela_linha");
+                    if (i%2==0)
+                        out.print(" tabela_linha_cor");
+                    out.println("\">");
+                    out.println("<td>" + i + "</td>");
+                    out.print("<td>");
+                    if (saldo_dev>0)
+                        out.println(NumberFormat.getCurrencyInstance().format(saldo_dev));
+                    out.println("</td>");
+                    out.println("<td>"+NumberFormat.getCurrencyInstance().format(amort)+"</td>");
+                    out.println("<td>"+NumberFormat.getCurrencyInstance().format(juros)+"</td>");
+                    out.println("<th>"+NumberFormat.getCurrencyInstance().format(prest)+"</th>");
+                    out.println("</tr>");
+                }
+                out.println("</table>");
+            }
+            out.println("</div>");
             out.println("</body>");
             out.println("</html>");
         }
